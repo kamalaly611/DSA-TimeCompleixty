727bc5d5e2886a03531a4622635f4a818b4fdcea
kamalaly611 <etopiyaaly611@gmail.com>
2025-03-08 01:15:18 +0500

    Insert, pop ,find,clear

--- DSA-SLO-3.py
+++ DSA-SLO-3.py
@@ -21,9 +21,15 @@ class Mylist:
         #[1,2,3]
         result=''
         for i in range(self.n):
-            result=result+str(self.A[i]) + ','
-        
+            result=result+str(self.A[i]) + ','# loop iterates through ith to n-1  
+        # what if we don't have empty string in intial so the last result will be
+        #
         return '[' + result[:-1] + ']'
+    def __getitem__(self,index):
+        if 0<=index<self.n:
+            return self.A[index]
+        else:
+            return 'index is out of range'
 
         
     def append(self,item): # if the size and n are equal than  the array will be resizes:::
@@ -34,6 +40,30 @@ class Mylist:
         #append 
          self.A[self.n] = item 
          self.n = self.n + 1
+    def pop(self):
+        if self.n==0:
+            return '[]'
+        print(self.A[self.n-1 ])
+        self.n=self.n-1
+    def clear(self):
+        self.n=0
+        self.size=1
+    def find(self,item):
+        for i in range(self.n):
+            if self.A[i]==item:
+                return i
+        return 'ValueError -Not in List'
+    def insert(self,pos,item):
+        if self.n==self.size:
+            self._resize(self.size*2)
+        
+        for i in range(self.n,pos,-1):
+            self.A[i]=self.A[i-1]
+        
+        self.A[pos]=item
+        self.n=self.n+1
+
+    
     
     def _resize(self,new_capcity):
         #crete a new array with  new capcity
@@ -56,8 +86,14 @@ L.append(False)
 L.append(4.5)
 L.append('Hey')
 
+# print(L)
+# print(L[4])
+print(L.pop())
+print(L)    
+#print(L.clear())
 print(L)
-    
-
-        
-        
+print(L.find(4.5))        
+print(L.insert(0,0))
+print(L)        
+print(L.insert(1,'Khalid'))
+print(L)        
